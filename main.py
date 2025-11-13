2440749b4486ad3ed29e648a4c0c821c5d6a044d
ale <alemoura.zyc@gmail.com>
2025-11-12 22:44:01 -0300

    feat: created function to get workout name

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 class Menu():
   main_menu_messages = {
       'welcome': '-== welcome! ==-\n',
-      'options': '1. Create workout\n2. Edit workout\n3. Delete workout\n4. Exit\n\n'
+      'options': '1. Create workout\n2. Edit workout\n3. Delete workout\n4. List workouts\n5. Exit\n\n'
     }
+  
   def __init__(self):
     self.loop_menu()
 
@@ -14,8 +15,11 @@ class Menu():
       user_option = input(Menu.main_menu_messages['options'])
       user_option = user_option.lower().strip()
 
+      print()
+
       if user_option in ['1', 'create', 'create workout']:
-        print('create workout')
+        create = Create_workout()
+        create.workout_infos['name'] = create.ask_workout_name()
         break
       elif user_option in ['2', 'edit', 'edit workout']:
         print('edit workout')
@@ -23,12 +27,54 @@ class Menu():
       elif user_option in ['3', 'delete', 'delete workout']:
         print('delete workout')
         break
-      elif user_option in ['4', 'exit']:
+      elif user_option in ['4', 'list', 'list workout']:
+        print('exit')
+        break
+      elif user_option in ['5', 'exit']:
         print('exit')
         break
       else:
         print('invalid option')
         break
 
+  
+class Create_workout():
+  create_workout_messages = {
+    'name': 'What is the name of the workout?\n\n',
+    'name_error': 'Workout name cannot be empty!\n',
+    #'days': '0) Monday\n1) Tuesday\n2) Wednesday\n3) Thursday\n4) Friday\n5) Saturday\n6) Sunday\n\n',
+    'choose_day': 'Choose the day:\n',
+    'choose_day_or_done': 'Choose another day or type [d]one: '
+  }
+
+  def __init__(self):
+    self.workout_infos = {}
+      # 'name': '',
+      # 'days': {
+      #   'monday': {
+      #     'name': '',
+      #     'exercises': [
+      #       {'name': '', 'series': '', 'reps': ''}
+      #     ]
+      #   },
+      #   'tuesday': {},
+      #   'wednesday': {},
+      #   'thursday': {},
+      #   'friday': {},
+      #   'saturday': {},
+      #   'sunday': {}
+      # }
+
+  def ask_workout_name(self):
+    name = ''
+    while not name:
+      name_typing = input(self.create_workout_messages['name'])
+      if len(name_typing) < 1:
+        print(self.create_workout_messages['name_error'])
+        continue
+      else:
+        name = name_typing.strip().title()
+    return name
+    
 
 Menu()
